83d231ab26e0126308ab579102c8dba045e4bc93
shahzorhossain <shazorkhaled14@gmail.com>
2018-08-05 19:10:20 -0400

    first commit

---
+++ client/components/Single.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class Single extends Component {
+  render() {
+    return <div className="single-photo"> I'm a single</div>;
+  }
+}
---
+++ client/stack.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import css from './styles/style.styl';
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
+);
+ReactDOM.render(router, document.getElementById('root'));
